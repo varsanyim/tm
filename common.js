30d2a5069ebe45ab3c456db8dac9e6bed5c30ac8
varsanyim <6471221+varsanyim@users.noreply.github.com>
2026-07-19 14:47:56 +0200

    Create common.js

---
+++ common.js
@@ -0,0 +1,26 @@
+// common.js
+(function () {
+    'use strict';
+    function cleanText(element) {
+        if (!element) return '';
+
+        return element.textContent
+            .replace(/\s+/g, ' ')
+            .trim();
+    }
+
+    function isTypingTarget(element) {
+        if (!element) return false;
+
+        const tagName = element.tagName?.toLowerCase();
+
+        return (
+            tagName === 'input' ||
+            tagName === 'textarea' ||
+            tagName === 'select' ||
+            element.isContentEditable
+        );
+    }
+
+  
+})();
